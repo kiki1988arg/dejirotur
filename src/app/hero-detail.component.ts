75b5fc7090671fe817b0e0c3f8bf7756a75eb984
kiki1988arg <ezequielarakaki@hotmail.com>
2017-04-05 15:29:18 -0300

    HASTA Refactor routes to a Routing Module

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,6 +1,12 @@
-import { Component , Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from './hero.service';
 
 import { Hero } from './hero';
+
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'hero-detail',
@@ -12,9 +18,24 @@ import { Hero } from './hero';
         <label>name: </label>
         <input [(ngModel)]="hero.name" placeholder="name"/>
       </div>
+      <button (click)="goBack()">Back</button>
     </div>
   `
 })
-export class HeroDetailComponent {
-    @Input() hero: Hero;
+export class HeroDetailComponent implements OnInit {
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
+  @Input() hero: Hero;
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+  goBack(): void {
+    this.location.back();
+  }
 }
