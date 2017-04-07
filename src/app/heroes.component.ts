92383a18382a9903c322ccbe88ead354291ed643
kiki1988arg <ezequielarakaki@hotmail.com>
2017-04-06 22:58:37 -0300

    HTTP

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'my-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
-  
+
 })
 export class HeroesComponent implements OnInit {
-  constructor(private heroService: HeroService) { };
+  constructor  (
+    private heroService: HeroService,
+    private router: Router
+    ) 
+  { };
 
 
   title = 'Tour of Heroes';
@@ -19,10 +24,13 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
   getHeroes(): void {
-     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 
-   ngOnInit(): void {
+  ngOnInit(): void {
     this.getHeroes();
+  }
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 }
